c03c4c66e983c88971542d14d1716d2faf4cffb8
Andrew Carter <andrewcarter1992@gmail.com>
2015-10-02 10:26:51 +0100

    PSR-2 on naming and associative array changes

--- check.php
+++ check.php
@@ -5,7 +5,7 @@ require_once 'keys.php';
 
 use Wunderlist\WunderlistClient as Wunderlist;
 
-$wunderlist = new Wunderlist($client_id, $access_token);
+$wunderlist = new Wunderlist($clientId, $accessToken);
 
 echo deliver($wunderlist);
 
@@ -16,7 +16,7 @@ function deliver($wunderlist) {
     }
 
     $response = $wunderlist->completeTask($_POST['task_id'], $_POST['revision']);
-    if ($response->revision === (int) $_POST['revision'] + 1 && $response->completed === true) {
+    if ($response['revision'] === (int) $_POST['revision'] + 1 && $response['completed'] === true) {
         return json_encode(['completed' => true]);
     }
 
