b7cf8ef327c2bf73223f5e8e3f5b9fe10b650d42
Andrew Carter <andrewcarter1992@gmail.com>
2015-10-02 10:24:13 +0100

    Updates to exception handling
    Cleaned up exception handling. I think the best thing to do is let Guzzle exceptions fall through rather than catch them and destroy the information that they hold.
    
    In some sense, the response status code check might be unnecessary. Guzzle will already convert 4XX and 5XX error codes into exceptions for us, but I think it's still good practice to perform our own check. A 3XX code could signify that the operation hasn't completed so we would want an exception to be raised here.
    
    I've also:
    - Added a bit more consistency to the parameter checks
    - Consistent naming of parameter variables
    - Expanded a bit on the phpdocs.
    - Added an extra parameter to the json_decode calls so that it returns an associative array rather than an anonymous object

--- src/WunderlistClient.php
+++ src/WunderlistClient.php
@@ -4,6 +4,7 @@ namespace Wunderlist;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
+use Psr\Http\Message\ResponseInterface;
 
 class WunderlistClient
 {
@@ -23,99 +24,123 @@ class WunderlistClient
     }
 
     /**
+     * Check the response status code.
+     * 
+     * @param ResponseInterface $response           The response from the guzzle HTTP call
+     * @param int               $expectedStatusCode The expected status code
+     * 
+     * @throws \RuntimeException On unexpected status code
+     */
+    private function checkResponseStatusCode(ResponseInterface $response, $expectedStatusCode)
+    {
+        $statusCode = $response->getStatusCode();
+
+        if ($statusCode !== $expectedStatusCode) {
+            throw new \RuntimeException('Wunderlist API returned status code ' . $statusCode . ' expected ' . $expectedStatusCode);
+        }
+    }
+
+    /**
      * Returns all the lists
      *
-     * @return mixed
+     * @return array
      */
     public function getLists()
     {
-        try {
-            $response = $this->guzzle->get('lists');
-        } catch(ClientException $e) {
-            return array();
-        }
-        return json_decode($response->getBody());
+        $response = $this->guzzle->get('lists');
+
+        $this->checkResponseStatusCode($response, 200);
+
+        return json_decode($response->getBody(), true);
     }
 
     /**
      * Returns a specific list
      *
-     * @param $id
+     * @param int $listId The list id
      *
-     * @return mixed
+     * @return array
      */
-    public function getList($id)
+    public function getList($listId)
     {
-        if (!is_numeric($id)) {
-            return NULL;
+        if (!is_numeric($listId)) {
+            throw new \InvalidArgumentException('The list id must be numeric');
         }
 
-        try {
-            $response = $this->guzzle->get('lists/' . $id);
-        }
-        catch (ClientException $e) {
-            return NULL;
-        }
+        $response = $this->guzzle->get('lists/' . $listId);
 
-        return json_decode($response->getBody());
+        $this->checkResponseStatusCode($response, 200);
+
+        return json_decode($response->getBody(), true);
     }
 
     /**
      * Return all the tasks of a given list
      *
-     * @param $list_id
+     * @param int $listId The list id
      *
-     * @return array()
+     * @return array
      */
-    public function getListTasks($list_id)
+    public function getListTasks($listId)
     {
-        if (!$list_id) {
-            return array();
+        if (!is_numeric($listId)) {
+            throw new \InvalidArgumentException('The list id must be numeric');
         }
 
-        try {
-            $response = $this->guzzle->get('tasks', ['query' => ['list_id' => $list_id]]);
-        }
-        catch (ClientException $e) {
-            return array();
-        }
+        $response = $this->guzzle->get('tasks', ['query' => ['list_id' => $listId]]);
 
-        return json_decode($response->getBody());
+        $this->checkResponseStatusCode($response, 200);
+
+        return json_decode($response->getBody(), true);
     }
 
     /**
      * Creates a new task
      *
-     * @param $task
-     * @return mixed
+     * See: https://developer.wunderlist.com/documentation/endpoints/task
+     *
+     * @param string $name       The name of the task
+     * @param int    $listId     The id of the list to add the task to
+     * @param array  $parameters Other task parameters
+     * 
+     * @return array
      */
-    public function createTask($task)
+    public function createTask($name, $listId, array $parameters)
     {
-        try {
-            $response = $this->guzzle->post('tasks', ['body' => json_encode($task)]);
-        }
-        catch (ClientException $e) {
-            return false;
+        if (!is_numeric($listId)) {
+            throw new \InvalidArgumentException('The list id must be numeric');
         }
 
-        return json_decode($response->getBody());
+        $parameters['name']   = $name;
+        $parameters['listId'] = $listId;
+
+        $response = $this->guzzle->post('tasks', ['body' => json_encode($parameters)]);
+
+        $this->checkResponseStatusCode($response, 201);
+
+        return json_decode($response->getBody(), true);
     }
 
     /**
      * Completes a task
      *
-     * @param $task_id
-     * @param $revision
-     * @return mixed
+     * @param int $taskId
+     * @param int $revision
+     * 
+     * @return array
      */
-    public function completeTask($task_id, $revision)
+    public function completeTask($taskId, $revision)
     {
-        try {
-            $response = $this->guzzle->patch('tasks/' . $task_id, ['body' => json_encode(['revision' => (int) $revision, 'completed' => true])]);
-        } catch (ClientException $e) {
-            return false;
+        if (!is_numeric($taskId)) {
+            throw new \InvalidArgumentException('The list id must be numeric');
+        } elseif (!is_numeric($revision)) {
+            throw new \InvalidArgumentException('The revision must be numeric');
         }
 
-        return json_decode($response->getBody());
+        $response = $this->guzzle->patch('tasks/' . $taskId, ['body' => json_encode(['revision' => (int) $revision, 'completed' => true])]);
+
+        $this->checkResponseStatusCode($response, 200);
+
+        return json_decode($response->getBody(), true);
     }
 }
